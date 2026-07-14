c622c83d51143275a65cc8c9000313632fff1798
Vedansh Kumar Srivastava <vedansh1310@gmail.com>
2026-07-14 22:33:12 +0530

    Initialize FastAPI backend

---
+++ backend/services/predictor.py
@@ -0,0 +1,5 @@
+import joblib
+
+model = joblib.load("C:\\Users\\HP\\AI Projects\\Smart-House-Price-Prediction\\models\\gradient_boosting.pkl")
+
+scaler = joblib.load("C:\\Users\\HP\\AI Projects\\Smart-House-Price-Prediction\\models\\scaler.pkl")
